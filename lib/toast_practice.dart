2b2dbd774ce3ad36c81de92eea660ed3f536882a
unknown <sonsy@smsoftlab.co.kr>
2024-02-15 14:41:10 +0900

    first commit

---
+++ lib/toast_practice.dart
@@ -0,0 +1,9 @@
+library toast_practice;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  static int addOne(int value) => value + 1;
+
+  static int minusOne(int value) => value -1;
+}
---
+++ test/toast_practice_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:toast_practice/toast_practice.dart';
+
+void main() {
+  test('adds one to input values', () {
+    //final calculator = Calculator();
+    expect(Calculator.addOne(2), 3);
+    expect(Calculator.addOne(-7), -6);
+    expect(Calculator.addOne(0), 1);
+  });
+}
